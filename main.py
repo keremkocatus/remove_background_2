305b87db305ca78cf35b5d47ea803617c237212f
Kerem Kocatuş <keremkocatus17@gmail.com>
2025-05-25 00:53:35 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+from rembg import new_session, remove
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import StreamingResponse
+from contextlib import asynccontextmanager
+from PIL import Image
+from io import BytesIO
+
+session = None
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    global session
+    session = new_session(model_name="u2net")
+    yield
+    
+app = FastAPI(lifespan=lifespan)
+
+@app.post("/process-image")
+async def process_image(
+    photo: UploadFile = File(...),
+    clothing: UploadFile = File(...),
+):
+    
+    contents = await photo.read()
+    img = Image.open(BytesIO(contents))
+    
+    processed_image = remove(img, session=session)
+    
+    buf = BytesIO()
+    processed_image.save(buf, format="jpg")
+    buf.seek(0)
+    
+    return StreamingResponse(buf, media_type="image/jpg")
+
+    
+    
