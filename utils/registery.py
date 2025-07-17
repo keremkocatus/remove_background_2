0f3cb89d58f29ebe1ff5a873dd812a8232ba41e2
Mücahit Can <108959400+ShinyPantss@users.noreply.github.com>
2025-07-17 21:33:26 +0300

    Refactor job status handling in registery.py to include wardrobe_id in the response and improve readability with consistent formatting.

--- utils/registery.py
+++ utils/registery.py
@@ -3,6 +3,7 @@ from fastapi import HTTPException
 
 JOB_REGISTRY: dict[str, dict] = {}
 
+
 # Register a new background-removal job and return its identifier
 def register_job(
     image_url: str,
@@ -28,16 +29,20 @@ def register_job(
         "rembg_url": None,
         "enhance_url": None,
     }
+    print(JOB_REGISTRY)
     return job_id
+
 
 def get_job_by_id(job_id):
     return JOB_REGISTRY.get(job_id)
+
 
 def get_job_id_by_job(job: dict):
     for jid, record in JOB_REGISTRY.items():
         if record is job or record == job:
             return jid
     raise ValueError("No job found matching the provided job dict")
+
 
 def get_job_by_prediction_id(prediction_id: str, is_enhance: bool) -> tuple[str, dict]:
     for job_id, job in JOB_REGISTRY.items():
@@ -50,15 +55,17 @@ def get_job_by_prediction_id(prediction_id: str, is_enhance: bool) -> tuple[str,
 
     raise ValueError(f"No job found with prediction ID: {prediction_id}")
 
+
 def update_registry(job_id: str, key: str, new_value):
     job = JOB_REGISTRY.get(job_id)
     if not job:
         raise ValueError(f"No job found with job ID: {job_id}")
-    
+
     if key not in job:
         raise KeyError(f"Key '{key}' not found in the job with ID: {job_id}")
-    
+
     job[key] = new_value
+
 
 def get_job_status(job_id: str, is_enhance: bool):
     job = JOB_REGISTRY.get(job_id)
@@ -66,18 +73,29 @@ def get_job_status(job_id: str, is_enhance: bool):
         raise HTTPException(status_code=404, detail=f"Job {job_id} bulunamadı")
 
     if is_enhance:
-        if job["enhance_status"] == "finished" and job["rembg_status"] == "finished" and job["caption_status"] == "finished":
+        if (
+            job["enhance_status"] == "finished"
+            and job["rembg_status"] == "finished"
+            and job["caption_status"] == "finished"
+        ):
             result_url = job["rembg_url"]
+            wardrobe_id = job["wardrobe_id"]
+            print(wardrobe_id)
             del JOB_REGISTRY[job_id]
 
-            return {"job_id": job_id, "status": "finished", "result_url": result_url}
+            return {
+                "job_id": job_id,
+                "status": "finished",
+                "result_url": result_url,
+                "wardrobe_id": wardrobe_id,
+            }
         else:
             return {"job_id": job_id, "status": "processing"}
     else:
         if job["rembg_status"] == "finished" and job["caption_status"] == "finished":
             result_url = job["rembg_url"]
             del JOB_REGISTRY[job_id]
-            
+
             return {"job_id": job_id, "status": "finished", "result_url": result_url}
         else:
             return {"job_id": job_id, "status": "processing"}
